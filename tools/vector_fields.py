2448948f8e9047815854564d68778bf900c5a0fd
Jeremias Epperlein <jeremias.epperlein@gmail.com>
2023-04-16 23:02:20 +0200

    Fine tuning Pendel

--- tools/vector_fields.py
+++ tools/vector_fields.py
@@ -57,7 +57,6 @@ def field_player(f,width=1300,height=700):
     vec2 v = vec2(0., 0.);
     float x = p.x;
     float y = p.y;
-    float r2 = x*x+y*y;
     v.x = {to_glsl(sympy.N(f[0]))};
     v.y = {to_glsl(sympy.N(f[1]))};
     return v;
@@ -85,8 +84,8 @@ def plot_vector_field(g,var1,var2, numpoints=20, ax=None, **args):
     #ax.spines['bottom'].set_position('center')
     #ax.spines['right'].set_color('none')
     #ax.spines['top'].set_color('none')
-    ax.hlines(0,var1[1],var1[2])
-    ax.vlines(0,var2[1],var2[2])
+    ax.hlines(0,var1[1],var1[2],color="black")
+    ax.vlines(0,var2[1],var2[2],color="black")
     ax.set_xlim(var1[1],var1[2])
     ax.set_ylim(var2[1],var2[2])
     ax.quiver(X,Y,U,V, **args)
@@ -109,8 +108,8 @@ def plot_streamlines(g,var1,var2,numpoints=100, ax=None, **args):
     #ax.spines['bottom'].set_position('center')
     #ax.spines['right'].set_color('none')
     #ax.spines['top'].set_color('none')
-    ax.hlines(0,var1[1],var1[2])
-    ax.vlines(0,var2[1],var2[2])
+    ax.hlines(0,var1[1],var1[2],color="black")
+    ax.vlines(0,var2[1],var2[2],color="black")
     ax.set_xlim(var1[1],var1[2])
     ax.set_ylim(var2[1],var2[2])
     ax.streamplot(X,Y,U,V,**args)
@@ -140,6 +139,15 @@ def plot_solution_pair(f,variables,t1=18.0,x_0=0.2,y_0=0):
     solution = solve_ivp(f,variables,(0,t1),(x_0,y_0))
     ax1.plot(solution.t,solution.y[0,:])
     ax2.plot(solution.t,solution.y[1,:],color="red")
+    return fig
+
+def plot_solution_non_autonomous(f, time_var, space_vars,initial_value, t_1=12):
+    solution = solve_non_autonomous_ivp(f,time_var, space_vars,(0,t_1),initial_value)
+    fig = plt.figure()
+    ax = fig.subplots(1,1)
+    ax.plot(solution.t,solution.y[0,:])
+    fig.set_figwidth(16)
+    fig.set_figheight(5)
     return fig
 
 def norm(f):
